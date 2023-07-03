5eb4f10b1827d3fa4fd6d05b979634dd09fdb8d3
Lidkin <lidkin79@gmail.com>
2023-07-03 13:28:31 +0300

    added image

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$('.loop').owlCarousel({
+    center: true,
+    items: 2,
+    loop: true,
+    margin: 0,
+    responsive: {
+        800: {
+            items: 3
+        }
+    }
+});
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
